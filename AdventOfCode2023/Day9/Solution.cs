4f345039c7cb7f95f3b34cd02a866c99c3f50965
Kevin Andres <kevin.andres@marcusmillichap.com>
2023-12-09 15:45:00 -0700

    Update day 7 to be a bit smarter

--- AdventOfCode2023/Day9/Solution.cs
+++ AdventOfCode2023/Day9/Solution.cs
@@ -7,7 +7,7 @@ public class Solution : SolutionBase
     protected override string PartOneInputFile { get; init; } = "9_1.txt";
     protected override string PartTwoInputFile { get; init; } = "9_1_sample.txt";
 
-    private long Recurse(long[] values, bool forwards = true)
+    private long GetNextInSequence(long[] values)
     {
         if (values.All(v => v == 0))
             return 0;
@@ -17,18 +17,16 @@ public class Solution : SolutionBase
         for (var i = 0; i < values.Length - 1; i++)
             decompositions[i] = values[i + 1] - values[i];
 
-        var delta = Recurse(decompositions, forwards);
+        var delta = GetNextInSequence(decompositions);
 
-        return forwards
-            ? values[values.Length - 1] + delta
-            : values[0] - delta;
+        return values[values.Length - 1] + delta;
     }
 
     public override long PartOne()
     {
         var result = GetFileContents(PartOneInputFile)
             .Select(l => l.ParseLongs().ToArray())
-            .Select(x => Recurse(x, true))
+            .Select(GetNextInSequence)
             .Sum();
 
         return result;
@@ -37,8 +35,8 @@ public class Solution : SolutionBase
     public override long PartTwo()
     {
         var result = GetFileContents(PartOneInputFile)
-            .Select(l => l.ParseLongs().ToArray())
-            .Select(x => Recurse(x, false))
+            .Select(l => l.ParseLongs().Reverse().ToArray())
+            .Select(GetNextInSequence)
             .Sum();
 
         return result;
